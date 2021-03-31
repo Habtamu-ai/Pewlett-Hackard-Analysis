bc1e4e3c692baf772496f4ecb3d540718775250d
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-03-31 19:48:00 -0400

    Adding Retirment information.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -5,6 +5,17 @@ CREATE TABLE departments(
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
+CREATE TABLE dept_emp(
+     emp_no INT NOT NULL,
+	dept_no VARCHAR(8) NOT NULL,
+     from_date DATE NOT NULL,
+     to_date DATE NOT NULL,
+     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+     PRIMARY KEY (emp_no,dept_no)
+
+);
 CREATE TABLE employees(
 	   emp_no INT NOT NULL,
 	   birth_date DATE NOT NULL,
