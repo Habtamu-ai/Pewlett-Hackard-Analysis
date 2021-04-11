d565973e82828df6ee236be1d26c0d531286d333
Habtamu Molla <78656720+Habtamu-ai@users.noreply.github.com>
2021-04-11 18:21:00 -0400

    Adding mentorship eligiblity

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,4 +28,20 @@ FROM unique_titles As u
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
-
+--create a Mentorship Eligibility
+SELECT DISTNICT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t._title
+INTO mentorship_eligiblity
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON(e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON(e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
